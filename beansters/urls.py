28274a27d631392ee3f101dc336be825aa5bcc07
tomkelly111 <thomaspkelly111@gmail.com>
2023-08-08 10:00:44 +0000

    add user login functionality

--- beansters/urls.py
+++ beansters/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('coffeeposts.urls'), name='coffeeposts_url;')
+    path('', include('coffeeposts.urls'), name='coffeeposts_url;'),
+    path('accounts/', include('allauth.urls')),
 ]
