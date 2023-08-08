ae1ad1940a3797e9094b99fb0b5c90142fe26863
tomkelly111 <thomaspkelly111@gmail.com>
2023-08-08 14:03:34 +0000

    add post detail view

--- coffeeposts/urls.py
+++ coffeeposts/urls.py
@@ -1,6 +1,8 @@
 from . import views
 from django.urls import path
+from .views import PostDetail
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home' )
+    path('', views.PostList.as_view(), name='home' ),
+    path('post/<str:shop>/', PostDetail.as_view(), name='post_detail')
 ]
--- coffeeposts/views.py
+++ coffeeposts/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404  
+from django.views import generic, View
 from .models import CoffeeShopPost
 
 
@@ -10,3 +10,16 @@ class PostList(generic.ListView):
     paginate_by = 3
 
 # Create your views here.
+class PostDetail(View):
+    def get(self, request, shop, *args, **kwargs):
+        queryset = CoffeeShopPost.objects
+        post = get_object_or_404(queryset, shop=shop)
+
+        return render(
+            request,
+            "post_detail.html", {
+                "post": post,
+                "shop": shop
+            }
+        )
+
