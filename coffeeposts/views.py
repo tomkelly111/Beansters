9b11bf3bf1959365ba268253ccc2d3c915e12a42
tomkelly111 <thomaspkelly111@gmail.com>
2023-08-10 13:25:12 +0000

    delete unused code from views.py

--- coffeeposts/views.py
+++ coffeeposts/views.py
@@ -53,32 +53,6 @@ class PostDetail(View):
         )
 
 
-    def post(self, request, shop, *args, **kwargs):
-        queryset = CoffeeShopPost.objects
-        post = get_object_or_404(queryset, shop=shop)
-        comments = post.comments.filter(approved=True).order_by("-created_on")
-
-        comment_form = CommentForm(data=request.POST)
-
-        if comment_form.is_valid():
-            comment_form.instance.email = request.user.email
-            comment_form.instance.name = request.user.username
-            comment = comment_form.save(commit=False)
-            comment.post = post
-            comment.save()
-        else:
-            comment_form = CommentForm()
-
-
-        return render(
-            request,
-            "post_detail.html", {
-                "post": post,
-                "commented": True,
-                "comments": comments,
-                "comment_form": CommentForm()
-            }
-        )
     
     
 def post_approval_view(request):
