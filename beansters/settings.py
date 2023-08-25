b87438b9d81cc84eae99720cd3557790c090d316
tomkelly111 <thomaspkelly111@gmail.com>
2023-08-25 19:44:46 +0000

    set debug to false

--- beansters/settings.py
+++ beansters/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-tomkelly111-beansters-eitrrmoipi7.ws-eu104.gitpod.io',
@@ -107,15 +107,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'beansters.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
      'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
  }
